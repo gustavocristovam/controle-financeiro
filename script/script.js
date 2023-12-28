dd96b15060ac730dadf44c5771a312e10ff25566
Gustavo Cristovam <71038617+gustavocristovam@users.noreply.github.com>
2023-12-28 15:41:53 -0300

    js att

--- script/script.js
+++ script/script.js
@@ -4,3 +4,8 @@ let modal = document.querySelector('div.modal-overlay')
 
 modal.setAttribute('class', `${modal.getAttribute('class')} active`)
 }
+
+function cancel() {
+    var modal = document.querySelector('div.modal-overlay')
+    modal.setAttribute('class', `modal-overlay`)
+}
